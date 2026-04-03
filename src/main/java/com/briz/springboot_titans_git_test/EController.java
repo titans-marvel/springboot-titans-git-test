3da8f65ba16ed0af90c32c5fc6a6b9f02503b2c4
Adnan Amir <adnanamirrnc@gmail.com>
2026-04-03 10:49:50 +0530

    this is my new changes to controller

---
+++ src/main/java/com/briz/springboot_titans_git_test/EController.java
@@ -0,0 +1,16 @@
+package com.briz.springboot_titans_git_test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class EController {
+
+@RequestMapping("/adnan")
+
+public String adnan() {
+	return "Hellow this is Adnan from Briztech";
+}
+
+}c
