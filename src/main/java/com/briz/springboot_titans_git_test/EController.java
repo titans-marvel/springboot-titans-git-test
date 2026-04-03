0292ce0dfaab9482bfaddc41f6388c393b9d7e23
mukesh <you@example.com>
2026-04-03 11:15:28 +0530

    i have created a new end point

---
+++ src/main/java/com/briz/springboot_titans_git_test/EController.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_titans_git_test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EController {
+
+		@RequestMapping("/mukesh")
+	public String hello(){
+		return "Hello World!";
+	}
+}
