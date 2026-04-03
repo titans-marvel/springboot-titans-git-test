bf313c52a5273606a15f098774e43d8261a560f7
kanchan raju <kanchanraju2015@gmail.com>
2026-04-03 10:33:39 +0530

    first commit of titans

---
+++ src/main/java/com/briz/springboot_titans_git_test/SpringbootTitansGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_titans_git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTitansGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTitansGitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briz/springboot_titans_git_test/SpringbootTitansGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_titans_git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTitansGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
